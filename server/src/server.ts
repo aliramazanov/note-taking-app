5fe3e622bd10bfced8aac4291ed8dff1e5a0d181
Ali Ramazanov <aliasifzade@gmail.com>
2023-10-26 15:49:22 +0400

    Connect Database & Prisma ORM

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+
+app.get("/api/notes", async (req, res) => {
+  const notes = await prisma.note.findMany();
+  res.json(notes);
+});
 
 const port = process.env.port;
 
